b0efce6b1dba53aba7dfb2353772908d8200f25b
stef03codes <129624887+stef03codes@users.noreply.github.com>
2025-01-29 22:43:26 +0100

    Added the video

--- Homework3/stock-microservice/src/main/java/com/analysis/stockmicroservice/StockMicroserviceApplication.java
+++ Homework3/stock-microservice/src/main/java/com/analysis/stockmicroservice/StockMicroserviceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockMicroserviceApplication.class, args);
 	}
